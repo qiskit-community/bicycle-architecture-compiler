6ca1952e1b0ef3f710ec59ee09ceeb4569e00ff0
Eddie Schoute <eddieschoute@gmail.com>
2025-02-03 17:23:03 -0500

    Clarify comments

--- src/bicycle_isa/src/lib.rs
+++ src/bicycle_isa/src/lib.rs
@@ -1,30 +1,34 @@
 pub enum Pauli {
     I,
     X,
-    Y,
     Z,
+    Y,
 }
 
 pub enum BicycleISA {
-    SyndromeCycle,                 // Syndrome cycle
-    CSSInitZero,                   // Initialize the block in |0>^12
-    CSSInitPlus,                   // Initialize the block in |+>^12
-    DestructiveZ,                  // Measure all qubits in Z and infer logical Z measurements
-    DestructiveX,                  // Meaasure all qubits in X and infer logical X measurements
-    Automorphism { x: u8, y: u8 }, // Automorphism generators.
+    SyndromeCycle, // Syndrome cycle
+    CSSInitZero,   // Initialize the block in |0>^12
+    CSSInitPlus,   // Initialize the block in |+>^12
+    DestructiveZ,  // Measure all qubits in Z and infer logical Z measurements
+    DestructiveX,  // Measure all qubits in X and infer logical X measurements
+    // Automorphism generators with x in {0,...,5} and y in {0,1,2} and x+y>0
+    Automorphism { x: u8, y: u8 },
 
     // Measurements
-    Measure { p1: Pauli, p7: Pauli }, // Measure qubits 1 and 7 with specified Paulis
-    JointMeasure { p1: Pauli, p7: Pauli }, // Measure qubits 1 and 7 in a joint operation with another block
-    ParallelMeasure { p1p7: Pauli }, // Independently measure qubit 1 and qubit 7 in the same basis simultaneously.
+    // Measure qubits 1 and 7 with specified Paulis, one of which must not be identity
+    Measure { p1: Pauli, p7: Pauli },
+    // Measure qubits 1 and 7 in a joint operation with another block, one of which must not be identity.
+    JointMeasure { p1: Pauli, p7: Pauli },
+    // Independently measure qubit 1 and qubit 7 in the X or the Z basis
+    ParallelMeasure { p1p7: Pauli },
 
     // Entanglement between two blocks
     JointBellInit, // Initialize two codes into 12 Bell states via rotating donut method
     JointTransversalCX, // Transversal CX using rotating donut
 
     // Magic
-    InitT,                             // Initialization into 8 physical-noise |T> states
-    TGate { basis: Pauli, qubit: u8 }, // Specify either X, X', Z, or Z'.
+    InitT,                                // Initialization into 8 physical-noise |T> states
+    TGate { basis: Pauli, primed: bool }, // Apply exp(iπ/8 P), with P in {X, X', Z, Z'}
 }
 
 #[cfg(test)]
