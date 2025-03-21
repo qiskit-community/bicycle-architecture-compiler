251d9e382a60958a780993bd9ff139e2ef9b2711
Eddie Schoute <eddie.schoute@ibm.com>
2025-03-21 10:29:42 -0400

    Test that qubit measurements are native

--- src/gross_code_cliffords/src/lib.rs
+++ src/gross_code_cliffords/src/lib.rs
@@ -6,3 +6,28 @@ mod tableau;
 pub use pauli_rotation::PauliString;
 
 pub use decomposition::{CompleteMeasurementTable, MeasurementTableBuilder};
+
+#[cfg(test)]
+mod tests {
+    use std::sync::LazyLock;
+
+    use super::*;
+    use native_measurement::NativeMeasurement;
+
+    static MEASUREMENT_IMPLS: LazyLock<CompleteMeasurementTable> = LazyLock::new(|| {
+        let mut builder = MeasurementTableBuilder::new(NativeMeasurement::all());
+        builder.build();
+        builder
+            .complete()
+            .expect("Generating a complete measurement table should succeed")
+    });
+
+    #[test]
+    fn qubit_measurements_are_native() {
+        for i in 0..24 {
+            let p: PauliString = PauliString(1 << i);
+            let (_, rots) = MEASUREMENT_IMPLS.implementation(p);
+            assert_eq!(0, rots.len());
+        }
+    }
+}
