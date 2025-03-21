b9d6464e4d0f467a17201b6d48af81ddf59aba47
Eddie Schoute <eddie.schoute@ibm.com>
2025-03-21 15:24:34 -0400

    Implement random generators for Paulis and Measurements

--- src/benchmark/src/random.rs
+++ src/benchmark/src/random.rs
@@ -4,23 +4,47 @@ use pbc_gross::language::PbcOperation;
 use rand::{distr::Uniform, prelude::*};
 
 pub fn random_measurements(qubits: usize) -> impl Iterator<Item = PbcOperation> {
-    std::iter::repeat(0).map(move |_| PbcOperation::Measurement {
-        basis: random_paulis(qubits),
-        flip_result: false,
-    })
-}
-
-pub fn random_paulis(length: usize) -> Vec<Pauli> {
     let mut rng = rand::rng();
     let range = Uniform::new_inclusive(0, 3).unwrap();
 
-    (0..length)
-        .map(|_| match range.sample(&mut rng) {
-            0 => Pauli::I,
-            1 => Pauli::X,
-            2 => Pauli::Z,
-            3 => Pauli::Y,
-            _ => unreachable!("RNG number out of range"),
-        })
-        .collect()
+    // I'm not sure how I can compose random_paulis() with mapping to a Measurement;
+    // I run into borrow issues.
+    std::iter::repeat_with(move || {
+        let paulis: Vec<_> = (0..qubits)
+            .map(|_| match range.sample(&mut rng) {
+                0 => Pauli::I,
+                1 => Pauli::X,
+                2 => Pauli::Z,
+                3 => Pauli::Y,
+                _ => unreachable!("RNG number out of range"),
+            })
+            .collect();
+
+        PbcOperation::Measurement {
+            basis: paulis,
+            flip_result: false,
+        }
+    })
+}
+
+pub fn random_paulis() -> impl Iterator<Item = Pauli> + 'static {
+    let mut rng = rand::rng();
+    let range = Uniform::new_inclusive(0, 3).unwrap();
+
+    std::iter::repeat_with(move || match range.sample(&mut rng) {
+        0 => Pauli::I,
+        1 => Pauli::X,
+        2 => Pauli::Z,
+        3 => Pauli::Y,
+        _ => unreachable!("RNG number out of range"),
+    })
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_rand_paulis() {}
 }
