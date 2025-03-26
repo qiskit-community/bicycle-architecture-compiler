faec3dc71e5eb6c20e846398aa880e12cda517ab
Eddie Schoute <eddie.schoute@ibm.com>
2025-03-26 14:20:56 -0400

    Compute depth

--- src/numerics/src/main.rs
+++ src/numerics/src/main.rs
@@ -20,6 +20,15 @@ impl InstructionCounter {
             Instruction::JointMeasure(_) => self.joint_measurements += 1,
         }
     }
+
+    fn max(self, other: InstructionCounter) -> Self {
+        Self {
+            rotations: self.rotations.max(other.rotations),
+            automorphisms: self.automorphisms.max(other.automorphisms),
+            measurements: self.measurements.max(other.measurements),
+            joint_measurements: self.joint_measurements.max(other.joint_measurements),
+        }
+    }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -37,14 +46,43 @@ fn main() -> Result<(), Box<dyn Error>> {
         pbc_gross::optimize::remove_duplicate_measurements_chunked(compiled_measurements);
 
     let data_blocks = architecture.data_blocks();
-    println!("i,qubits,blocks,rotations,automorphisms,measurements,joint measurements");
+    println!(
+        "i,qubits,blocks,rotations,automorphisms,measurements,joint measurements,cumulative measurement depth"
+    );
 
+    let mut depths: Vec<u64> = vec![Default::default(); data_blocks];
     for (i, meas_impl) in optimized_measurements.enumerate() {
         let mut counter: InstructionCounter = Default::default();
         // Accumulate counts. Or use a fold.
         meas_impl.iter().for_each(|instr| counter.add(&instr[0].1));
+
+        // Compute the new depths for each block
+        for op in meas_impl {
+            // Update the InstructionCounter of the involved blocks to be the max
+            if op.len() > 1 {
+                let mut max_counter: u64 = Default::default();
+                for (block_i, _) in op.iter() {
+                    max_counter = max_counter.max(depths[*block_i]);
+                }
+                for (block_i, _) in op.iter() {
+                    depths[*block_i] = max_counter;
+                }
+            }
+
+            // Add the instruction to each block
+            for (block_i, instr) in op {
+                match instr {
+                    Instruction::Measure(_) | Instruction::JointMeasure(_) => depths[block_i] += 1,
+                    _ => {}
+                }
+            }
+        }
+
+        // Calculate the max depth currently
+        let measurement_depth = depths.iter().reduce(|a, b| a.max(b)).unwrap();
+
         println!(
-            "{},{},{},{},{},{},{}",
+            "{},{},{},{},{},{},{},{}",
             i + 1,
             qubits,
             data_blocks,
@@ -52,6 +90,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             counter.automorphisms,
             counter.measurements,
             counter.joint_measurements,
+            measurement_depth,
         );
     }
 
