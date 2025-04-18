d1872099efb69fb8e8355a3bea82c4f8815b84ca
Eddie Schoute <eddie.schoute@ibm.com>
2025-04-18 15:56:20 -0400

    Add streaming json output to benchmark

--- src/benchmark/src/main.rs
+++ src/benchmark/src/main.rs
@@ -1,42 +1,36 @@
-use core::str;
-use std::{error::Error, io};
+use std::{
+    error::Error,
+    io::{self, Write},
+};
 
 use log::debug;
+use serde::{
+    ser::{SerializeSeq, Serializer},
+    Serialize,
+};
+
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    qubits: usize,
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     env_logger::init();
 
-    let args: Vec<_> = std::env::args().collect();
-    let qubits = str::parse::<usize>(&args[1])?;
-    assert!(qubits > 0);
-    let measurements = benchmark::random::random_measurements(qubits);
+    let cli = Cli::parse();
+    assert!(cli.qubits > 0);
+    let mut measurements = benchmark::random::random_measurements(cli.qubits);
 
-    let mut builder = csv::WriterBuilder::new();
-    builder.has_headers(false);
-    let mut writer = builder.from_writer(io::stdout());
-    for measurement in measurements {
-        let mut out = vec![String::from("m")];
-        out.push(
-            measurement
-                .basis()
-                .iter()
-                .map(|ps| ps.to_string())
-                .collect::<Vec<_>>()
-                .join(""),
-        );
-        out.push(String::from("+"));
-
-        // If I/O failed, just quit gracefully.
-        // Could happen when pipe was closed (e.g. head -n 10)
-        if let Err(e) = writer.write_record(out) {
-            debug!("Error when writing record: {e}");
-            if e.is_io_error() {
-                break;
-            } else {
-                return Err(Box::new(e));
-            }
-        }
-    }
+    let mut stdout = io::stdout();
+    // Stop on first error
+    let err = measurements.try_for_each(|measurement| {
+        let mut out = serde_json::to_string(&measurement)?;
+        out.push('\n');
+        stdout.write_all(out.as_bytes())
+    });
+    debug!("Encountered error while writing to stdout: {:?}", err);
 
     Ok(())
 }
