7077550921c49bb307914c6370da0623b8500769
Eddie Schoute <eddie.schoute@ibm.com>
2025-05-05 15:50:05 -0400

    Update script to generate data multiple times

--- generate_data.sh
+++ generate_data.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 set -euo pipefail
 
-models=("gross 1e-3 121" "gross 1e-3 1353" \
-# "gross 1e-3 13728" \
-    "gross 1e-4 110" "gross 1e-4 1342" "gross 1e-4 13717" \
-    "two-gross 1e-3 55" "two-gross 1e-3 704" \
-# "two-gross 1e-3 7150" \
-    "two-gross 1e-4 440" "two-gross 1e-4 6886"    )
+# Build binaries
+cargo build --release
 
-for i in "${models[@]}"
-do
-    set -- $i
-    cargo run --release --package random_numerics -- --model $1 --noise $2 --qubits $3 \
-        > "out_$1_$2_$3.csv" &
-done
-wait
+# Cache measurement tables
+# ./target/release/pbc_gross gross generate table_gross
+# ./target/release/pbc_gross two-gross generate table_two-gross
+
+# Read parameters from parameters.csv and run each parameter 10 times
+parallel --colsep "," "./target/release/random_numerics --model {1} --noise {2} --qubits {3} --measurement-table table_{1} > out_{1}_{2}_{3}_{4}.csv" \
+     :::: parameters.csv \
+     ::: $(seq 1 10)
+
 echo "Data generation complete. Concatenating output."
-
 awk '(NR == 1) || (FNR > 1)' out_*.csv > data.csv
-# rm out_*.csv
+rm out_*.csv
