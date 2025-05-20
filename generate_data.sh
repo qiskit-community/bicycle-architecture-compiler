8a2d0bc21872ed4ab16eaf920b4df5c283ebc239
Eddie Schoute <eddie.schoute@ibm.com>
2025-05-20 14:57:40 -0400

    Remove useless accuracy

--- generate_data.sh
+++ generate_data.sh
@@ -9,7 +9,7 @@ cargo build --release
 # ./target/release/pbc_gross two-gross generate table_two-gross
 
 # Read parameters from parameters.csv and run each parameter 10 times
-parallel --colsep "," "./target/release/random_numerics --model {1} --noise {2} --qubits {3} --measurement-table table_{1} --accuracy 5e-11 > out_{1}_{2}_{3}_{4}.csv" \
+parallel --colsep "," "./target/release/random_numerics --model {1} --noise {2} --qubits {3} --measurement-table table_{1} > out_{1}_{2}_{3}_{4}.csv" \
      :::: parameters.csv \
      ::: $(seq 1 10)
 
