9a3d59ff27a5862965027f6d49d80468a4d094e7
John Lapeyre <jlapeyre@users.noreply.github.com>
2025-07-01 15:45:13 -0400

    Another small comment

--- scripts/run_random_numerics.sh
+++ scripts/run_random_numerics.sh
@@ -2,19 +2,18 @@
 set -euo pipefail
 
 ##
-## This script is only necessary in case run_random_numerics.py fails, for some
-## reason. We do not expect that it will fail.
 ## This script is a simpler alternative to run_random_numerics.py
 ## It does the same thing that run_random_numerics.py does
 ##
 ## This script should be run from generate_data.sh.
-## It should not be run directly.
+## It need not be run directly.
 ##
 ## This script requires the utility GNU parallel
 
 # Change to this script's directory
 cd "$(dirname "$0")" || exit
 
+# This data was computed and written by the executable ./target/release/pbc_gross
 input_data_dir="../data"
 
 parallel --no-notice --colsep "," \
