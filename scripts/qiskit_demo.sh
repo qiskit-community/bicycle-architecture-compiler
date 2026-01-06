ec5b7e4e8282d76f444f38935e5244fde6e76543
Julien Gacon <jules.gacon@googlemail.com>
2026-01-06 18:46:36 +0100

    Fix copyright + table locations

--- scripts/qiskit_demo.sh
+++ scripts/qiskit_demo.sh
@@ -1,12 +1,22 @@
-#! /bin/sh
+#!/bin/sh
+# Copyright contributors to the Bicycle Architecture Compiler project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 set -eo pipefail
 
-# Change to this script's directory
+# Change to this script's directory.
 cd "$(dirname "$0")" || exit
-
-# path to the bicycle compiler executables
-COMPILER_PATH=../target/release
 
 if [ -z $1 ]; then
     N=100
@@ -15,11 +25,15 @@ else
     N=$1
 fi
 
-CODE="two-gross"
-P="1e-4" 
+COMPILER_PATH=../target/release # path to the bicycle compiler executables
+INPUT_DATA_DIR="../data" # path to store measurement tables
+CODE="two-gross" # type of code, can be "gross" or "two-gross"
+P="1e-4" # physical error rate, can be "1e-3" or "1e-4"
 
-# we'll force pre-generating the Clifford table, since that takes a long time
-MEASUREMENT_TABLE="table_${CODE}.dat"
+# We'll force pre-generating the Clifford table for the specified code, since that takes a long 
+# time. You can also generate the tables for both gross and two-gross code using 
+# the generate_measurement_tables.sh script.
+MEASUREMENT_TABLE="${INPUT_DATA_DIR}/table_${CODE}.dat"
 if [[ ! -f ${MEASUREMENT_TABLE} ]]; then
     echo "${MEASUREMENT_TABLE} not found, generating it..."
     ${COMPILER_PATH}/bicycle_compiler ${CODE} generate ${MEASUREMENT_TABLE} || {
@@ -28,9 +42,9 @@ if [[ ! -f ${MEASUREMENT_TABLE} ]]; then
     }
 fi
 
-# we yield the PBC instruction from the Python script and then consume them in the bicycle
+# We yield the PBC instruction from the Python script and then consume them in the bicycle
 # compiler to yield Gross code instruction, which are then consumed by the numerics to produce
-# error estimates
+# error estimates.
 python qiskit_demo.py $N \
-    | ${COMPILER_PATH}/bicycle_compiler ${CODE} --measurement-table table_${CODE}.dat \
+    | ${COMPILER_PATH}/bicycle_compiler ${CODE} --measurement-table ${MEASUREMENT_TABLE} \
     | ${COMPILER_PATH}/bicycle_numerics $N ${CODE}_$P
